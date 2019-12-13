369b4dc124244ff48924536aae45c9e139e176f2
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2019-12-13 11:13:53 +0300

    fix string formatting

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyyolo',
-    version='0.1.1',
-    description='Python Darknet YOLO Wrapper',
+    version='0.1.3',
+    description='Easy to use Python wrapper for YOLO Real-Time Object\
+Detection Library',
     author = 'Göktuğ Karakaşlı',
     author_email='karakasligk@gmail.com',
     license='MIT',
     url='https://github.com/goktug97/PyYOLO',
-    download_url='https://github.com/goktug97/PyYOLO/archive/v0.1.tar.gz',
+    download_url='https://github.com/goktug97/PyYOLO/archive/v0.1.3.tar.gz',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['pyyolo'],
